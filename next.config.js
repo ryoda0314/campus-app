3888f779ccb9d6f348739210598da15eaa90d591
ryoda0314 <edamamemochi2004@gmail.com>
2025-12-29 20:42:28 +0900

    feat: implement PWA support

--- next.config.js
+++ next.config.js
@@ -16,7 +16,7 @@ const nextConfig = {
 
 const withPWA = require("next-pwa")({
   dest: "public",
-  disable: false,
+  disable: process.env.NODE_ENV === "development",
   register: true,
   skipWaiting: true,
 });
